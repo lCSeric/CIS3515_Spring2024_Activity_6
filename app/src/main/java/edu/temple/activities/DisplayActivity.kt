d387018588b96a66473708cd997741aa9a4670be
lCSeric <133724439+lCSeric@users.noreply.github.com>
2024-02-22 12:51:47 -0500

    Update DisplayActivity.kt

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -10,8 +10,10 @@ class DisplayActivity : AppCompatActivity() {
         setContentView(R.layout.activity_display)
 
         // TODO Step 3: Extract transferred value and use for lyricsDisplayView text size
+        val textSize = intent.getIntExtra("textSize", 0)
         with (findViewById<TextView>(R.id.lyricsDisplayTextView)) {
-
+            val lyricsDisplayView = findViewById<TextView>(R.id.lyricsDisplayTextView)
+            lyricsDisplayView.textSize = textSize.toFloat()
         }
 
 
